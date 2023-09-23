8c94e264fe78eebc7f833fd25597c464fd68bef9
ZacMacd <zacmacdonald2010@gmail.com>
2023-09-23 13:30:07 -0400

    feat: create initial files for the Chat UI and Logic

---
+++ chat.py
@@ -0,0 +1,7 @@
+"""
+This file will contain the code for the Chat Logic.
+Messages will be received from app.py and a response 
+will be generated and returned. 
+Will need to incorporate the use of some AI chat interface
+API.    
+"""
